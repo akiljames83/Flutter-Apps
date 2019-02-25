75441b1068723e32bd995472f5eb97420fe51626
Akil Hamilton <akil.james83@gmail.com>
2019-02-25 13:01:16 -0500

    Modularizing code

--- demoapp/lib/main.dart
+++ demoapp/lib/main.dart
@@ -1,48 +1,20 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 // Render the widget
 // Following is format for one line functions
 void main() => runApp(MyApp());
 
 // Defining a widget; inherits from a base class from flutter sdk
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = [
-    'Food Tester',
-  ];
-
+class MyApp extends StatelessWidget {
   @override // overides method built into the widget
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('EasyList')),
-        body: Column(children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () {
-                setState(() {
-                  _products.add('Advanced Food Tester');
-                }); 
-            }, 
-            child: Text('Add Product')),
-          ),
-          Column(
-              children: _products
-                  .map((element) => Card(
-                          child: Column(children: <Widget>[
-                        Image.asset('assets/apple.png'),
-                        Text(element)
-                      ])))
-                  .toList()),
-        ]),
-      ),
+          appBar: AppBar(title: Text('EasyList')), 
+          body: ProductManager(),
+        ),
     );
   }
 }
