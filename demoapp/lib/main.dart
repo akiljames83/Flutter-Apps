f28aae015a9b9898f419966fb3dd16ad2aee86fd
Akil Hamilton <akil.james83@gmail.com>
2019-02-25 01:06:01 -0500

    Edits

--- demoapp/lib/main.dart
+++ demoapp/lib/main.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 
-main() {
-  // Render the widget
-  runApp(MyApp());
-}
+// Render the widget
+// Following is format for one line functions
+void main() => runApp(MyApp());
 
 // Defining a widget; inherits from a base class from flutter sdk
-class MyApp extends StatelessWidget {
-  // Defining the build method 
-  build(context) {
-    /* 
-    * MaterialApp: Core root widget!
-    * Scaffold: Blank Page
-    * AppBar: Application bar at the top of the screen
-    * Text: takes text as an argument so that it canbe displayed
-    */
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = [
+    'Food Tester',
+  ];
+
+  @override // overides method built into the widget
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList')
+        appBar: AppBar(title: Text('EasyList')),
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _products.add('Advanced Food Tester');
+                }); 
+            }, 
+            child: Text('Add Product')),
           ),
-        ),
-      );
+          Column(
+              children: _products
+                  .map((element) => Card(
+                          child: Column(children: <Widget>[
+                        Image.asset('assets/apple.png'),
+                        Text(element)
+                      ])))
+                  .toList()),
+        ]),
+      ),
+    );
   }
 }
